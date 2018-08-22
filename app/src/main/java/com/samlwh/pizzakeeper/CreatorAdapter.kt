29b5e2a909702c3759f00e031bdcfbb616410f14
samlwh <samlwh@hotmail.com>
2018-08-22 16:21:43 +0800

    init project
    init project

---
+++ app/src/main/java/com/samlwh/pizzakeeper/CreatorAdapter.kt
@@ -0,0 +1,40 @@
+package com.samlwh.pizzakeeper
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.TextView
+import com.samlwh.pizzakeeper.data.Topping
+
+class CreatorAdapter(val pizzaView: PizzaView): RecyclerView.Adapter<CreatorAdapter.ViewHolder>() {
+    val list = mutableListOf<Topping>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.topping_row, parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.updateView(position)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.textView)
+        val switch = view.findViewById<Switch>(R.id.toggle)
+
+        fun updateView(index: Int) {
+            val topping = list[index]
+
+            switch.setOnCheckedChangeListener { button, isChecked ->
+              pizzaView.toppings[topping] = isChecked
+              pizzaView.invalidate()
+            }
+
+            textView.text = topping.name
+        }
+    }
+}
---
+++ app/src/main/java/com/samlwh/pizzakeeper/data/Topping.kt
@@ -0,0 +1,7 @@
+package com.samlwh.pizzakeeper.data
+
+data class Topping (
+    val id: Int,
+    val name: String,
+    val drawableName: String
+)
