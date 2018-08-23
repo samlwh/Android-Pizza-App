de8bdc2e5c8b8214b218267803a9791fd22406fd
samlwh <samlwh@hotmail.com>
2018-08-23 11:09:27 +0800

    Creating the RoomDatabase

--- app/src/main/java/com/samlwh/pizzakeeper/MainActivity.kt
+++ app/src/main/java/com/samlwh/pizzakeeper/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.samlwh.pizzakeeper
 
+import android.arch.persistence.room.Room
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.samlwh.pizzakeeper.data.PizzaDatabase
 
 
 const val PIZZA_ID = "PIZZA_ID"
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val db = Room.databaseBuilder(this, PizzaDatabase::class.java, "PizzaDatabase").build()
+        //db.pizzaDao().
 
         recyclerView = findViewById(R.id.pizzaRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
