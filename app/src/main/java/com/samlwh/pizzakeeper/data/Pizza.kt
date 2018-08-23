9999bbf3d3685437ce5d77a4b43bff87475ff869
samlwh <samlwh@hotmail.com>
2018-08-23 10:32:57 +0800

    Adding Room Library and assign entity

--- app/src/main/java/com/samlwh/pizzakeeper/data/Pizza.kt
+++ app/src/main/java/com/samlwh/pizzakeeper/data/Pizza.kt
@@ -1,8 +1,13 @@
 package com.samlwh.pizzakeeper.data
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import java.util.*
 
+@Entity
 data class Pizza (
+
+    @PrimaryKey
     val id: Int,
     val name: String,
     val creationDate: Date
--- app/src/main/java/com/samlwh/pizzakeeper/data/Topping.kt
+++ app/src/main/java/com/samlwh/pizzakeeper/data/Topping.kt
@@ -1,6 +1,13 @@
 package com.samlwh.pizzakeeper.data
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
 data class Topping (
+
+
+    @PrimaryKey
     val id: Int,
     val name: String,
     val drawableName: String
