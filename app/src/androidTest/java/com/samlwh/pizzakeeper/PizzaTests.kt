9a9ddf32aa90ef5325bc22ef73269285c96c8d39
samlwh <samlwh@hotmail.com>
2018-08-23 11:53:50 +0800

    save which Toppings belong on which Pizza into the PizzaTopping table

--- app/src/androidTest/java/com/samlwh/pizzakeeper/PizzaTests.kt
+++ app/src/androidTest/java/com/samlwh/pizzakeeper/PizzaTests.kt
@@ -5,6 +5,7 @@ import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.samlwh.pizzakeeper.data.Pizza
 import com.samlwh.pizzakeeper.data.PizzaDatabase
+import com.samlwh.pizzakeeper.data.PizzaTopping
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,23 +25,43 @@ class PizzaTests {
 
     val testPizza = Pizza(0,  "Hawaiian", Date())
     val testToppingIds = listOf(1,7)
+    val appContext = InstrumentationRegistry.getTargetContext()
+    val db = Room.databaseBuilder(appContext, PizzaDatabase::class.java, "PizzaDatabase").build()
 
 
 
     @Test
-    fun useAppContext() {
+    fun pizzaTest() {
         // Context of the app under test.
 
         //Adding Pizza
-        val appContext = InstrumentationRegistry.getTargetContext()
-        val db = Room.databaseBuilder(appContext, PizzaDatabase::class.java, "PizzaDatabase").build()
-        
         //clear our database before start the test
         db.clearAllTables()
         db.pizzaDao().insert(testPizza)
         val returnedPizza = db.pizzaDao().getPizzaById(testPizza.id)
         assertEquals(testPizza, returnedPizza)
+    }
 
+    @Test
+    fun pizzaToppingTest() {
+        //Arrange
+        db.clearAllTables()
+        db.pizzaDao().insert(testPizza)
+        toppings.forEach {
+            db.toppingDao().insert(it)
+        }
+
+        //Act
+        testToppingIds.forEach{
+            val pizzaTopping = PizzaTopping(testPizza.id, it)
+            db.pizzaToppingDao().insert(pizzaTopping)
+        }
+        val returnToppingIds = db.pizzaToppingDao().getToppingIdsForPizzaId(testPizza.id)
+
+        //Assert
+        assertEquals(testToppingIds, returnToppingIds)
 
     }
+
+
 }
